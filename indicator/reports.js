15bc3d4fe85d20a453f70e6d71f16a930177fa37
Fan, Yugang <yugang.fan@intel.com>
2015-06-02 10:10:30 +0800

    Add js report file for jsonp

---
+++ indicator/reports.js
@@ -0,0 +1,289 @@
+{
+    "12.41.296.9": {
+        "platform": {
+            "android": {
+                "arch": {
+                    "arm": {
+                        "segment": {},
+                        "summary": {}
+                    },
+                    "x86": {
+                        "segment": {
+                            "Embedded Crosswalk": {
+                                "feature": {
+                                    "Embedding APIs XWalkUIClient": {},
+                                    "Embedding APIs XWalkView": {
+                                        "descriptions": "The test descriptions",
+                                        "hardware": "Asus MemoPad8",
+                                        "issues": {
+                                            "XWALK-1235": "https://crosswalk-project.org/jira/browse/XWALK-5678"
+                                        },
+                                        "passrate": "30",
+                                        "tests": {
+                                            "test-suite01-name": "https://github.com/crosswalk-project/crosswalk-test-suite",
+                                            "test-suite02-name": "https://github.com/crosswalk-project/crosswalk-test-suite"
+                                        },
+                                        "totalcasenum": "100"
+                                    },
+                                    "SIMD": {
+                                        "descriptions": "The test descriptions",
+                                        "hardware": "Asus MemoPad8",
+                                        "issues": {
+                                            "XWALK-2765": "https://crosswalk-project.org/jira/browse/XWALK-6789"
+                                        },
+                                        "passrate": "60",
+                                        "tests": {
+                                            "test-suite01-name": "https://github.com/crosswalk-project/crosswalk-test-suite",
+                                            "test-suite02-name": "https://github.com/crosswalk-project/crosswalk-test-suite"
+                                        },
+                                        "totalcasenum": "110"
+                                    }
+                                },
+                                "summary": {
+                                    "descriptions": "build test descriptions",
+                                    "fullreporturl": "http://www.baidu.com",
+                                    "hardware": "Asus MemoPad8",
+                                    "passrate": "70",
+                                    "totalcasenum": "10000"
+                                }
+                            },
+                            "Embedded Crosswalk WebView Plugin for Cordova": {
+                                "feature": {},
+                                "summary": {}
+                            },
+                            "Embedded Crosswalk based Cordova": {
+                                "feature": {},
+                                "summary": {}
+                            }
+                        },
+                        "summary": {
+                            "descriptions": "",
+                            "hardware": "",
+                            "passrate": "",
+                            "totalcasenum": ""
+                        }
+                    }
+                },
+                "summary": {
+                    "descriptions": "",
+                    "hardware": "",
+                    "passrate": "",
+                    "totalcasenum": ""
+                }
+            },
+            "linux": {
+                "arch": {
+                    "arm": {
+                        "segment": {},
+                        "summary": {}
+                    },
+                    "x86": {
+                        "segment": {
+                            "Embedded Crosswalk": {
+                                "feature": {
+                                    "Embedding APIs XWalkUIClient": {},
+                                    "Embedding APIs XWalkView": {
+                                        "descriptions": "The test descriptions",
+                                        "hardware": "NUC",
+                                        "issues": {
+                                            "XWALK-2354": "https://crosswalk-project.org/jira/browse/XWALK-5678"
+                                        },
+                                        "passrate": "70",
+                                        "tests": {
+                                            "test-suite01-name": "https://github.com/crosswalk-project/crosswalk-test-suite",
+                                            "test-suite02-name": "https://github.com/crosswalk-project/crosswalk-test-suite"
+                                        },
+                                        "totalcasenum": "80"
+                                    },
+                                    "SIMD": {
+                                        "descriptions": "The test descriptions",
+                                        "hardware": "NUC",
+                                        "issues": {
+                                            "XWALK-1876": "https://crosswalk-project.org/jira/browse/XWALK-6789"
+                                        },
+                                        "passrate": "60",
+                                        "tests": {
+                                            "test-suite01-name": "https://github.com/crosswalk-project/crosswalk-test-suite",
+                                            "test-suite02-name": "https://github.com/crosswalk-project/crosswalk-test-suite"
+                                        },
+                                        "totalcasenum": "40"
+                                    }
+                                },
+                                "summary": {
+                                    "descriptions": "build test descriptions",
+                                    "fullreporturl": "http://www.baidu.com",
+                                    "hardware": "NUC",
+                                    "passrate": "70",
+                                    "totalcasenum": "10000"
+                                }
+                            },
+                            "Embedded Crosswalk WebView Plugin for Cordova": {
+                                "feature": {},
+                                "summary": {}
+                            },
+                            "Embedded Crosswalk based Cordova": {
+                                "feature": {},
+                                "summary": {}
+                            }
+                        },
+                        "summary": {
+                            "descriptions": "",
+                            "hardware": "",
+                            "passrate": "",
+                            "totalcasenum": ""
+                        }
+                    }
+                },
+                "summary": {
+                    "descriptions": "",
+                    "hardware": "",
+                    "passrate": "",
+                    "totalcasenum": ""
+                }
+            },
+            "windows": {
+                "arch": {
+                    "arm": {
+                        "segment": {},
+                        "summary": {}
+                    },
+                    "x86": {
+                        "segment": {
+                            "Embedded Crosswalk": {
+                                "feature": {
+                                    "Embedding APIs XWalkUIClient": {},
+                                    "Embedding APIs XWalkView": {
+                                        "descriptions": "The test descriptions",
+                                        "hardware": "ASUS T100",
+                                        "issues": {
+                                            "XWALK-2567": "https://crosswalk-project.org/jira/browse/XWALK-5678"
+                                        },
+                                        "passrate": "20",
+                                        "tests": {
+                                            "test-suite01-name": "https://github.com/crosswalk-project/crosswalk-test-suite",
+                                            "test-suite02-name": "https://github.com/crosswalk-project/crosswalk-test-suite"
+                                        },
+                                        "totalcasenum": "60"
+                                    },
+                                    "SIMD": {
+                                        "descriptions": "The test descriptions",
+                                        "hardware": "ASUS T100",
+                                        "issues": {
+                                            "XWALK-8765": "https://crosswalk-project.org/jira/browse/XWALK-6789"
+                                        },
+                                        "passrate": "10",
+                                        "tests": {
+                                            "test-suite01-name": "https://github.com/crosswalk-project/crosswalk-test-suite",
+                                            "test-suite02-name": "https://github.com/crosswalk-project/crosswalk-test-suite"
+                                        },
+                                        "totalcasenum": "50"
+                                    }
+                                },
+                                "summary": {
+                                    "descriptions": "build test descriptions2222222222222222",
+                                    "fullreporturl": "http://www.baidu.com",
+                                    "hardware": "ASUS T100",
+                                    "passrate": "70",
+                                    "totalcasenum": "10000"
+                                }
+                            },
+                            "Embedded Crosswalk WebView Plugin for Cordova": {
+                                "feature": {},
+                                "summary": {}
+                            },
+                            "Embedded Crosswalk based Cordova": {
+                                "feature": {},
+                                "summary": {}
+                            }
+                        },
+                        "summary": {
+                            "descriptions": "",
+                            "hardware": "",
+                            "passrate": "",
+                            "totalcasenum": ""
+                        }
+                    }
+                },
+                "summary": {
+                    "descriptions": "",
+                    "hardware": "",
+                    "passrate": "",
+                    "totalcasenum": ""
+                }
+            },
+            "ios": {
+                "arch": {
+                    "arm": {
+                        "segment": {},
+                        "summary": {}
+                    },
+                    "x86": {
+                        "segment": {
+                            "Embedded Crosswalk": {
+                                "feature": {
+                                    "Embedding APIs XWalkUIClient": {},
+                                    "Embedding APIs XWalkView": {
+                                        "descriptions": "The test descriptions",
+                                        "hardware": "iPhone6 Plus",
+                                        "issues": {
+                                            "XWALK-1235": "https://crosswalk-project.org/jira/browse/XWALK-5678"
+                                        },
+                                        "passrate": "30",
+                                        "tests": {
+                                            "test-suite01-name": "https://github.com/crosswalk-project/crosswalk-test-suite",
+                                            "test-suite02-name": "https://github.com/crosswalk-project/crosswalk-test-suite"
+                                        },
+                                        "totalcasenum": "100"
+                                    },
+                                    "SIMD": {
+                                        "descriptions": "The test descriptions",
+                                        "hardware": "iPhone6 Plus",
+                                        "issues": {
+                                            "XWALK-2765": "https://crosswalk-project.org/jira/browse/XWALK-6789"
+                                        },
+                                        "passrate": "60",
+                                        "tests": {
+                                            "test-suite01-name": "https://github.com/crosswalk-project/crosswalk-test-suite",
+                                            "test-suite02-name": "https://github.com/crosswalk-project/crosswalk-test-suite"
+                                        },
+                                        "totalcasenum": "110"
+                                    }
+                                },
+                                "summary": {
+                                    "descriptions": "build test descriptions",
+                                    "fullreporturl": "http://www.baidu.com",
+                                    "hardware": "iPhone6 Plus",
+                                    "passrate": "70",
+                                    "totalcasenum": "10000"
+                                }
+                            },
+                            "Embedded Crosswalk WebView Plugin for Cordova": {
+                                "feature": {},
+                                "summary": {}
+                            },
+                            "Embedded Crosswalk based Cordova": {
+                                "feature": {},
+                                "summary": {}
+                            }
+                        },
+                        "summary": {
+                            "descriptions": "",
+                            "hardware": "",
+                            "passrate": "",
+                            "totalcasenum": ""
+                        }
+                    }
+                },
+                "summary": {
+                    "descriptions": "",
+                    "hardware": "",
+                    "passrate": "",
+                    "totalcasenum": ""
+                }
+            }
+        },
+        "summary": {}
+    },
+    "13.42.319.10": {},
+    "14.43.341.0": {}
+}
